0c508283c0a5e20dfae6c78071d36bb867033a16
victorhugobarbosa <victorhugobarbosa10000@gmail.com>
2023-10-25 10:57:56 -0300

    updates
    Cards hover, imgs

---
+++ MyCollege/js/script.js
@@ -0,0 +1,39 @@
+function aparecerTexto(txt, img){
+    var texto = document.getElementById(txt)
+    var imagem = document.getElementById(img)
+
+    var id = setInterval(frame, 10);
+    var imgPos = 0;
+    var txtPos = 20;
+
+    function frame() {
+        if (imgPos == -80) {
+            clearInterval(id);
+        } else {
+            imgPos -= 5
+            txtPos += 1
+            imagem.style.marginTop = imgPos + 'px';
+            texto.style.marginTop = txtPos + 'px'; 
+        }
+    }
+}
+
+function desaparecerTexto(txt, img){
+    var texto = document.getElementById(txt)
+    var imagem = document.getElementById(img)
+
+    var id = setInterval(frame, 10);
+    var imgPos = -80;
+    var txtPos = 36;
+
+    function frame() {
+        if (imgPos == 0) {
+            clearInterval(id);
+        } else {
+            imgPos += 5
+            txtPos -= 1
+            imagem.style.marginTop = imgPos + 'px';
+            texto.style.marginTop = txtPos + 'px'; 
+        }
+    }
+}
